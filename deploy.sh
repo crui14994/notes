d27a4fc6e4943338fefa26e6eedaaa6b351f477e
crui14994 <778943319@qq.com>
2019-11-26 22:02:39 +0800

    初始化

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:crui14994/notes.git master:gh-pages
+git push -f https://gitee.com/crui14994/notes.git master:gh-pages
 
 cd -
